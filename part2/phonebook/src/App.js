5a6d4e4039e7a062a68d8295c5dec6c6207b828f
Bossvanlindt <yann.bonzom11@gmail.com>
2022-07-10 23:50:23 +0100

    Phonebook with axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
   const [filter, newFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -41,7 +43,7 @@ function App() {
 
       <Filter
         value={filter}
-        onChange={(event) => newFilter(event.target.value)}
+        onChange={(event) => newFilter(event.target.value.toLowerCase())}
       />
 
       <h2>add a new</h2>
